22f3be6c29ea148611af8e6859e3c050fb56b501
Jack <iatenine@gmail.com>
2022-03-17 14:30:43 -0600

    Enable CrossOrigin
    CORS Shenanigans happening on Azure
    Death to CORS

--- src/main/java/com/revature/controllers/SquakController.java
+++ src/main/java/com/revature/controllers/SquakController.java
@@ -3,14 +3,12 @@ package com.revature.controllers;
 import com.revature.models.Squak;
 import com.revature.services.SquakService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class SquakController {
     @Autowired
     SquakService squakService;
