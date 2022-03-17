6ef080ae158818276510c66111a07c96065b85d5
Jack <iatenine@gmail.com>
2022-03-17 10:20:41 -0600

    DDL Generating
    Adapted application.properties to generate tables based on entities

--- src/main/java/com/revature/driver/SquakboxApplication.java
+++ src/main/java/com/revature/driver/SquakboxApplication.java
@@ -2,10 +2,14 @@ package com.revature.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.revature")
+@EntityScan("com.revature.models")
+@EnableJpaRepositories
 public class SquakboxApplication {
 
 	public static void main(String[] args) {
